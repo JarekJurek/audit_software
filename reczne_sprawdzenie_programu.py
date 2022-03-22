0991983806885dafccbfca9476b9c313bd5ea16a
kukoz25 <98827505+kukoz25@users.noreply.github.com>
2022-03-23 00:37:01 +0100

    Update reczne_sprawdzenie_programu.py
    Nowa wersja dodano
    - sprawdzanie po jsonie
    -  zmniejszono liczbę klawiszy do klikania
    - dopisano nazwę miecha do csv

--- reczne_sprawdzenie_programu.py
+++ reczne_sprawdzenie_programu.py
@@ -2,11 +2,12 @@ import os
 import cv2 as cv
 import numpy as np
 import csv
+import json
 
 def changing_dir_meat(): #zmienia ścieżkę w zależności od mięsa oraz zwraca ścieżkę wraz z nazwą mięsa
     meat_name=input('Podaj nazwe mięsa')
     meat_name=meat_name.strip()
-    path_main='C:\\Users\\produkcja\\Desktop\\dane\\'  #początek ścieżki absolutnej
+    path_main='D:\\praca\\'  #początek ścieżki absolutnej
     path=os.path.join(path_main, meat_name, 'results')
     os.chdir(path)
     #print(path)
@@ -40,11 +41,16 @@ def write_to_csv(path, meat_name, all_photos, tp, fp, tn, fn, fp_list, fn_list):
         fp_list=0
     if not fn_list:
         fn_list = 0
-    data=[all_photos, tp, fp, tn, fn, fp_list, fn_list]
-    with open(f'{path}\\{meat_name}_sprawdzenie_dzialania.csv', 'w', encoding='UTF8', newline='') as f:
+    data=[meat_name, all_photos, tp, fp, tn, fn, fp_list, fn_list]
+    with open(os.path.join(path,f'{meat_name}_sprawdzenie_dzialania.csv'),'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(data)
+        f.close()
 
+def jsonpath(path): #tworzy scieżke do jsona
+    json_name='data.json'
+    json_path=os.path.join(path, json_name)
+    return json_path
 
 
 def main():
@@ -74,36 +80,26 @@ def main():
         photos = dir_list(path)
         photos.sort(key=int)
         total_photo = dir_counter(path)
+        with open(jsonpath(os.path.join(path, photos[photo]))) as file:
+            results_data = json.load(file)
         #print(os.path.join(path, photos[photo]))
-        image_name0 = file_number_changer(0, os.path.join(path, photos[photo]))
-        image_name1 = file_number_changer(1, os.path.join(path, photos[photo]))
-        image_name2 = file_number_changer(2, os.path.join(path, photos[photo]))
-        image_name3 = file_number_changer(3, os.path.join(path, photos[photo]))
-        result_name   = os.path.join(path, photos[photo], 'result.jpg')
+        if os.path.exists(file_number_changer(4, os.path.join(path, photos[photo]))):
+            image_name0 = file_number_changer(4, os.path.join(path, photos[photo]))
+        else :
+            image_name0 = file_number_changer(2, os.path.join(path, photos[photo]))
         image0 = cv.imread(image_name0)
-        image1 = cv.imread(image_name1)
-        image2 = cv.imread(image_name2)
-        image3 = cv.imread(image_name3)
-        result = cv.imread(result_name)
-        # cv.imshow('syf1', image0)
-        # cv.imshow('syf2', image1)
-        # cv.imshow('syf3', image2)
-        # cv.imshow('syf4', image3)
-        # cv.imshow('Rezultat', result)
-        Verti0 = np.concatenate((image0, image1), axis=0)
-        Verti1 = np.concatenate((image2, image3,), axis=0)
-        width = int(Verti0.shape[1])
-        height = int(Verti0.shape[0])
-        dim = (width, height)
-        resized_result=resized = cv.resize(result, dim, interpolation = cv.INTER_AREA)
-        Hori = np.concatenate((Verti0, Verti1,resized_result), axis=1)
         cv.namedWindow("window", cv.WND_PROP_FULLSCREEN)
         cv.setWindowProperty("window", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
-        cv.imshow('window',Hori)
+        cv.imshow('window',image0)
         key = cv.waitKey(1)
         if key == ord('1'):
             all_photos += 1
-            tp += 1
+            results = results_data["count"]
+            if results == True:
+                tp += 1
+            elif results == False:
+                fn += 1
+                fn_list.append(f'{lines[line]}//{tests[test]}//{photos[photo]}')
             photo = photo + 1
             if photo == total_photo:
                 photo = 0
@@ -116,35 +112,12 @@ def main():
                     break
         elif key == ord('2'):
             all_photos += 1
-            fp += 1
-            fp_list.append(f'{lines[line]}//{tests[test]}//{photos[photo]}')
-            photo = photo + 1
-            if photo == total_photo:
-                photo = 0
-                test += 1
-                if test == total_tests:
-                    line += 1
-                    test = 0
-                    # print(line)
-                if line == total_lines:
-                    break
-        elif key == ord('3'):
-            all_photos += 1
-            tn += 1
-            photo = photo + 1
-            if photo == total_photo:
-                photo = 0
-                test += 1
-                if test == total_tests:
-                    line += 1
-                    test = 0
-                    # print(line)
-                if line == total_lines:
-                    break
-        elif key == ord('4'):
-            all_photos += 1
-            fn += 1
-            fn_list.append(f'{lines[line]}//{tests[test]}//{photos[photo]}')
+            results = results_data["count"]
+            if results == True:
+                fp += 1
+                fp_list.append(f'{lines[line]}//{tests[test]}//{photos[photo]}')
+            elif results == False:
+                tn += 1
             photo = photo + 1
             if photo == total_photo:
                 photo = 0
