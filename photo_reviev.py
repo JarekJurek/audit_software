22f34dfc9f7fc8d9ab6d5c66f07df757576f6533
Grzegorz <grzes.pawlak@gmail.com>
2022-04-11 17:04:21 +0200

    checkout box added

--- photo_reviev.py
+++ photo_reviev.py
@@ -3,6 +3,83 @@ import cv2 as cv
 import numpy as np
 import csv
 import json
+import tkinter as tk
+
+pollution_name = "empty"
+
+
+
+
+def input_box():
+    root = tk.Tk()
+    canvas1 = tk.Canvas(root, width=200, height=60)
+    canvas1.pack()
+
+    var1 = tk.IntVar()
+    var2 = tk.IntVar()
+    var3 = tk.IntVar()
+    var4 = tk.IntVar()
+    var5 = tk.IntVar()
+    var6 = tk.IntVar()
+    var7 = tk.IntVar()
+    var8 = tk.IntVar()
+    var9 = tk.IntVar()
+
+    R1 = tk.Checkbutton(root, text="False positive", variable=var1, onvalue=1, offvalue=0)
+    R1.pack(anchor=tk.W)
+
+    R2 = tk.Checkbutton(root, text="Zielony", variable=var2, onvalue=1, offvalue=0)
+    R2.pack(anchor=tk.W)
+
+    R3 = tk.Checkbutton(root, text="Niebieski", variable=var3, onvalue=1, offvalue=0)
+    R3.pack(anchor=tk.W)
+
+    R4 = tk.Checkbutton(root, text="Czarny", variable=var4, onvalue=1, offvalue=0)
+    R4.pack(anchor=tk.W)
+
+    R5 = tk.Checkbutton(root, text="Biały", variable=var5, onvalue=1, offvalue=0)
+    R5.pack(anchor=tk.W)
+
+    R6 = tk.Checkbutton(root, text="Szary", variable=var6, onvalue=1, offvalue=0)
+    R6.pack(anchor=tk.W)
+
+    R7 = tk.Checkbutton(root, text="Czerwony", variable=var7, onvalue=1, offvalue=0)
+    R7.pack(anchor=tk.W)
+
+    R8 = tk.Checkbutton(root, text="Żółty", variable=var8, onvalue=1, offvalue=0)
+    R8.pack(anchor=tk.W)
+
+    R9 = tk.Checkbutton(root, text="Pomarańczowy", variable=var9, onvalue=1, offvalue=0)
+    R9.pack(anchor=tk.W)
+
+    label = tk.Label(root)
+    label.pack()
+
+    def save_choice():
+        dataBase = {
+            "False positive": var1.get(),
+            "Zielony": var2.get(),
+            "Niebieski": var3.get(),
+            "Czarny": var4.get(),
+            "Biały": var5.get(),
+            "Szary": var6.get(),
+            "Czerwony": var7.get(),
+            "Żółty": var8.get(),
+            "Pomarańczowy": var9.get(),
+        }
+        for key, value in dataBase.items():
+            if value is not 0:
+                print(key)  # tą wartość (konkretny kolor) należy tutaj zpisać do pliku razem ze ścieżką
+        label1 = tk.Label(root, text="Zapisano!")
+        canvas1.create_window(100, 60, window=label1)  # output message box placement
+
+    buttonLabel = tk.Button(text='Zapisz', command=save_choice)
+    canvas1.create_window(70, 30, window=buttonLabel)  # button placement
+
+    quitButton = tk.Button(text='Wyjdź', command=root.destroy)
+    canvas1.create_window(130, 30, window=quitButton)  # button placement
+
+    root.mainloop()
 
 
 def changing_dir_meat():  # zmienia ścieżkę w zależności od mięsa oraz zwraca ścieżkę wraz z nazwą mięsa
@@ -75,8 +152,13 @@ def main():
         cv.setWindowProperty("window", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
         cv.putText(Hori, f"Pollution : {results}", (1000, 20), font, 0.5, color, 1)
         cv.imshow('window', Hori)
+
         key = cv.waitKey(1)
-        if key == ord('d'):
+
+        if key == ord('p'):
+            input_box()
+
+        elif key == ord('d'):
             photo = photo + 1
             if photo == total_photo:
                 photo = 0
