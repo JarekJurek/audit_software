cb75b97d7b3d0f05452a6a92facb20cba2b6cfde
kukoz25 <01150982@pw.edu.pl>
2022-04-11 11:58:08 +0200

    Update photo_reviev.py

--- photo_reviev.py
+++ photo_reviev.py
@@ -4,87 +4,90 @@ import numpy as np
 import csv
 import json
 
-def changing_dir_meat(): #zmienia ścieżkę w zależności od mięsa oraz zwraca ścieżkę wraz z nazwą mięsa
-    meat_name=input('Podaj nazwe mięsa')
-    meat_name=meat_name.strip()
-    path_main='E:\\dane\\'  #początek ścieżki absolutnej
-    path=os.path.join(path_main, meat_name, 'results')
+
+def changing_dir_meat():  # zmienia ścieżkę w zależności od mięsa oraz zwraca ścieżkę wraz z nazwą mięsa
+    meat_name = input('Podaj nazwe mięsa')
+    meat_name = meat_name.strip()
+    path_main = 'E:\\dane\\'  # początek ścieżki absolutnej
+    path = os.path.join(path_main, meat_name, 'results')
     os.chdir(path)
-    #print(path)
+    # print(path)
     return path, meat_name
 
 
-def dir_list(path): #ma zwracać listę folderów w folderze
+def dir_list(path):  # ma zwracać listę folderów w folderze
     list = os.listdir(path)
     return list
 
 
-def dir_counter(path): #liczy ilość foderów w ścieżce
-    TotalDir=0
+def dir_counter(path):  # liczy ilość foderów w ścieżce
+    TotalDir = 0
     for dirs in os.listdir(path):
         if os.path.isdir(path):
             TotalDir += 1
     return TotalDir
 
-def file_number_changer(image_number, path): #zmienia ścieżkę do pliku ze zdjeciem
-    image_name='base_image_'
-    image_extension='.jpg'
-    image_name=f'{image_name}{image_number}{image_extension}'
-    image_path=os.path.join(path,image_name)
-    #print(image_name)
-    #eturn image_name
+
+def file_number_changer(image_number, path):  # zmienia ścieżkę do pliku ze zdjeciem
+    image_name = 'base_image_'
+    image_extension = '.jpg'
+    image_name = f'{image_name}{image_number}{image_extension}'
+    image_path = os.path.join(path, image_name)
+    # print(image_name)
+    # eturn image_name
     return image_path
 
 
-def jsonpath(path): #tworzy scieżke do jsona
-    json_name='data.json'
-    json_path=os.path.join(path, json_name)
+def jsonpath(path):  # tworzy scieżke do jsona
+    json_name = 'data.json'
+    json_path = os.path.join(path, json_name)
     return json_path
 
 
 def main():
-    path, meat_name = changing_dir_meat() #nazwa mięsa oraz
-    pathog=path #ścieżka główna, która się nie zmienia
-    lines = dir_list(path) #lista linii
-    #print(lines)
-    total_lines = dir_counter(path) #liczba folderów z linią
-    #print(total_lines)
+    path, meat_name = changing_dir_meat()  # nazwa mięsa oraz
+    pathog = path  # ścieżka główna, która się nie zmienia
+    lines = dir_list(path)  # lista linii
+    # print(lines)
+    total_lines = dir_counter(path)  # liczba folderów z linią
+    # print(total_lines)
     line = 0  # potrzebne do chodzenia po listach
     photo = 0
     test = 0
     color = (255, 0, 0)
     font = cv.FONT_HERSHEY_SIMPLEX
-    #print(lines, tests, photos)
+    # print(lines, tests, photos)
     while True:
         path = os.path.join(pathog, lines[line], meat_name)
-        #print(path)
+        # print(path)
         tests = dir_list(path)
         total_tests = dir_counter(path)
-        print(total_tests)
-        #print(total_tests)
+        # print(total_tests)
+        path1 = path  # pomocnicza do cofania
         path = os.path.join(path, tests[test], '0')
         photos = dir_list(path)
         photos.sort(key=int)
         total_photo = dir_counter(path)
+        # print(total_photo)
         result_name = os.path.join(path, photos[photo], 'result.jpg')
         with open(jsonpath(os.path.join(path, photos[photo]))) as file:
             results_data = json.load(file)
         with open(jsonpath(os.path.join(path, photos[photo]))) as file:
             results_data = json.load(file)
-        #print(os.path.join(path, photos[photo]))
+        # print(os.path.join(path, photos[photo]))
         if os.path.exists(file_number_changer(4, os.path.join(path, photos[photo]))):
             image_name0 = file_number_changer(4, os.path.join(path, photos[photo]))
-        else :
+        else:
             image_name0 = file_number_changer(2, os.path.join(path, photos[photo]))
         image0 = cv.imread(image_name0)
         result = cv.imread(result_name)
-        Hori = np.concatenate((image0,result), axis=1) #łączyobrazki
+        Hori = np.concatenate((image0, result), axis=1)  # łączyobrazki
         results = results_data["count"]
-        #print(results)
+        # print(results)
         cv.namedWindow("window", cv.WND_PROP_FULLSCREEN)
         cv.setWindowProperty("window", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
-        cv.putText(Hori,f"Pollution : {results}",(1000,20),font,0.5,color,1)
-        cv.imshow('window',Hori)
+        cv.putText(Hori, f"Pollution : {results}", (1000, 20), font, 0.5, color, 1)
+        cv.imshow('window', Hori)
         key = cv.waitKey(1)
         if key == ord('d'):
             photo = photo + 1
@@ -96,22 +99,32 @@ def main():
                 test = 0
                 # print(line)
             if line == total_lines:
-                    break
-        elif key == ord('a'):
-            photo=photo-1
-            print(photo)
-            if photo ==-1:
-                test=test-1
-                print("test",test)
-            if test==-1:
-                photo =total_photo-1
-                line=line-1
-                print("line",line)
-                test = total_tests-1
-                print(test)
-            if line == -1:
-                print("Koniec foteczek")
+                print("koniec zdjec")
                 break
+        elif key == ord('a'):
+            photo = photo - 1
+            # print(photo)
+            if photo == -1:
+                test = test - 1
+                path1 = os.path.join(path1, tests[test], '0')
+                total_photo = dir_counter(path1)
+                photo = total_photo - 1
+                # print("test", test)
+                if test == -1:
+                    line = line - 1
+                    if line == -1:
+                        print("Koniec foteczek")
+                        break
+                    path2 = os.path.join(pathog, lines[line], meat_name)
+                    total_tests = dir_counter(path2)
+                    # print("line", line)
+                    test = total_tests - 1
+                    path3 = os.path.join(path2, tests[test], '0')
+                    total_photo = dir_counter(path3)
+                    photo = total_photo - 1
+                    # print("xd",photo,tests[test],lines[line],path3)
+
+
 
 
 
@@ -120,11 +133,6 @@ def main():
             break
 
     cv.destroyAllWindows()
-
-
-
-
-
 
 
 if __name__ == "__main__":
