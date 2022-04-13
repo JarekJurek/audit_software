dfc5163048cae3c004498aa000427b14850b729d
Jan Klimaszewski <jan.klimaszewski@pw.edu.pl>
2022-04-13 12:42:47 +0200

    working review_summary

--- review_summary.py
+++ review_summary.py
@@ -13,28 +13,38 @@ def main():
     
     meat_types = dir_list(data_path_main)
     for meat_type in meat_types:
-        
-        meat_type_pollutions_summary = {}
-        for pollution_type in pollution_database:
-            meat_type_pollutions_summary[pollution_type] = 0
 
-        series_path_list = get_series_path_list(meat_type, data_path_main)
-        for series_path in series_path_list:
-            series_labelled_file_path = os.path.join(series_path[1], 'series_labelled_metadata.json')
-            with open(series_labelled_file_path, 'r') as labelled_file:
-                series_labelled_metadata = json.load(labelled_file)
+        if os.path.isdir(os.path.join(data_path_main, meat_type)):      
             
-            for slm_index in range(1, len(series_labelled_metadata)+1):
-                slm_key = 'result_' + str(slm_index)
-                detected_pollutions = series_labelled_metadata[slm_key]["detected_pollutions"]
-                for detected_pollution in detected_pollutions:
-                    meat_type_pollutions_summary[detected_pollution['type']] += 1
-        
-        if meat_type in pollutions_summary:
-            for pollution_type in meat_type_pollutions_summary:
-                pollutions_summary[meat_type][pollution_type] += meat_type_pollutions_summary[pollution_type]
-        else:
-            pollutions_summary[meat_type] = meat_type_pollutions_summary
+            meat_type_pollutions_summary = {}
+            for pollution_type in pollution_database:
+                meat_type_pollutions_summary[pollution_type] = {}
+                meat_type_pollutions_summary[pollution_type]['True positive'] = 0
+                meat_type_pollutions_summary[pollution_type]['True negative'] = 0
+                meat_type_pollutions_summary[pollution_type]['False positive'] = 0
+                meat_type_pollutions_summary[pollution_type]['False negative'] = 0
+
+            series_path_list = get_series_path_list(meat_type, data_path_main)
+            for series_path in series_path_list:
+                series_labelled_file_path = os.path.join(series_path[1], 'series_labelled_metadata.json')
+                if os.path.exists(series_labelled_file_path):
+                    with open(series_labelled_file_path, 'r') as labelled_file:
+                        series_labelled_metadata = json.load(labelled_file)
+                    
+                    for slm_index in range(1, len(series_labelled_metadata)+1):
+                        slm_key = 'result_' + str(slm_index)
+                        detected_pollutions = series_labelled_metadata[slm_key]["detected_pollutions"]
+                        for detected_pollution in detected_pollutions:
+                            convusion_value = detected_pollution['confusion_value']
+                            pollution_type = detected_pollution['type']
+                            meat_type_pollutions_summary[pollution_type][convusion_value] += 1
+            
+                    if meat_type in pollutions_summary:
+                        for pollution_type in meat_type_pollutions_summary:
+                            for convusion_value in pollution_type['confusion_value']:
+                                pollutions_summary[meat_type][pollution_type][convusion_value] += meat_type_pollutions_summary[pollution_type][convusion_value]
+                    else:
+                        pollutions_summary[meat_type] = meat_type_pollutions_summary
 
     summary_file_path = os.path.join(data_path_main, 'series_labelled_metadata_summary.json')
     with open(summary_file_path, 'w') as summary_file:
